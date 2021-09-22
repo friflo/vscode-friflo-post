48526fa828eb19960ef983c018925e6f9b8a71d1
Ullrich Praetz <ullrich@praetz.de>
2021-09-22 17:21:58 +0200

    add POST file content to: ${endpoint}

--- src/CodelensProvider.ts
+++ src/CodelensProvider.ts
@@ -1,6 +1,8 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import { addCodelens as createCodelens } from './utils';
+import { configFileName, PostClientConfig } from './types';
+import { promises as fs } from 'fs';
 
 /**
  * CodelensProvider
@@ -16,12 +18,27 @@ export class CodelensProvider implements vscode.CodeLensProvider
         });
     }
 
-    public provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
+    public async provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.CodeLens[]> {
 
         if (vscode.workspace.getConfiguration("vscode-post-client").get("enablePostClient", true)) {
             const fileName  = path.normalize(document.fileName);
             if (fileName.endsWith("request.json")) {
                 const codeLenses = createCodelens(document);
+
+                const srcPath       = document.fileName;
+                const srcFolder     = path.dirname (srcPath) + "/";                
+                const configPath    = srcFolder + "/" + configFileName;
+                let   endpoint: string | null = null;
+                try {
+                    const configFile    = await fs.readFile(configPath,'utf8');
+                    const config        = JSON.parse(configFile) as PostClientConfig;
+                    endpoint            = config.endpoint;
+                    const entry = codeLenses[0];
+                    (entry as any)["endpoint"] = endpoint;   
+                }
+                catch (err) {
+                    // ignore error
+                }
                 return codeLenses;
             }            
         }
@@ -30,10 +47,15 @@ export class CodelensProvider implements vscode.CodeLensProvider
 
     public resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken) {
         if (vscode.workspace.getConfiguration("vscode-post-client").get("enablePostClient", true)) {
+            const   endpoint    = (codeLens as any)["endpoint"] as string | null;
+            let     tooltip     = `POST file content an REST API`;
+            if (endpoint) {
+                tooltip = `POST file content to: ${endpoint}`;
+            }
             codeLens.command = {
-                title: "POST",
-                tooltip: "POST file content to: ",
-                command: "vscode-post-client.codelensPost",
+                title:      "POST",
+                tooltip:    tooltip,
+                command:    "vscode-post-client.codelensPost",
                 // arguments: ["Argument 1", false]
             };
             return codeLens;
