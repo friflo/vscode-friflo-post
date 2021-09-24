1fab9a06793bf97227224b98b9d514026564caa5
Ullrich Praetz <ullrich@praetz.de>
2021-09-24 16:59:26 +0200

    show response info as plaintext

--- src/command-response-info/executeResponseInfo.ts
+++ src/command-response-info/executeResponseInfo.ts
@@ -7,6 +7,6 @@ import { languages, Uri, ViewColumn, window, workspace } from 'vscode';
 export async function executeResponseInfoPost (uri: Uri) {
     const doc = await workspace.openTextDocument(uri);
     // const list = await languages.getLanguages();
-    await languages.setTextDocumentLanguage(doc, "http");
+    await languages.setTextDocumentLanguage(doc, "plaintext");
     await window.showTextDocument(doc, ViewColumn.Active);
 }
