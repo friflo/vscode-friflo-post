5416d5705165ae7569bde885e880770c7431e14e
Ullrich Praetz <ullrich@praetz.de>
2021-09-25 09:48:13 +0200

    remove old response files

--- src/command-request/executeRequest.ts
+++ src/command-request/executeRequest.ts
@@ -10,6 +10,7 @@ import { configFileName, defaultConfigString, getConfigPath, getEndpoint, getHea
 import { ensureDirectoryExists,   getResponseInfoFromDestPathTrunk,   getWorkspaceFolder, openShowTextFile } from '../utils/vscode-utils';
 import { createHttpRequest, executeHttpRequest } from '../utils/http-got';
 import { getExtensionFromContentType } from '../utils/standard-content-types';
+import * as minimatch  from "minimatch";
 
 
 let requestCount = 0;
@@ -104,6 +105,7 @@ export async function executeRequest (requestType: RequestType, ...args: any[]) 
 
     const responseContent = getResponseFileContent(response);
 
+    await removeDestFiles(dstFolder, destPathTrunk);
     await fs.writeFile(responseContent.path, responseContent.content, 'utf8');
     // console.log(`saved: ${filePath}`);
     // open response ViewColumn.Beside to enable instant modification to request and POST again.
@@ -114,6 +116,17 @@ export async function executeRequest (requestType: RequestType, ...args: any[]) 
     // dont await    
     window.setStatusBarMessage(status, 10 * 1000);
     return response;
+}
+
+async function removeDestFiles (dstFolder: string, destPathTrunk: string) {
+    const filter = destPathTrunk.substring(dstFolder.length) + "*";
+    const list = await fs.readdir(dstFolder);
+    for (let i = 0; i < list.length; i++) {
+        const filePath = list[i];
+        if (minimatch(filePath, filter, { matchBase: true })) {
+            await fs.unlink(dstFolder + "/" + filePath);
+        }
+    }
 }
 
 function getResponseFileContent(responseData: ResponseData) : FileContent {
