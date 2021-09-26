56a67a4c42782ced713083328d5a6f4ee1b9c096
Ullrich Praetz <ullrich@praetz.de>
2021-09-26 08:51:11 +0200

    open markdown in Preview

--- src/command-response-info/executeResponseInfo.ts
+++ src/command-response-info/executeResponseInfo.ts
@@ -1,12 +1,16 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-import { languages, ViewColumn, window, workspace } from 'vscode';
+import { commands, Uri } from 'vscode';
 
 
 export async function executeResponseInfo (respPath: string) {
-    const doc = await workspace.openTextDocument(respPath);
-    // const list = await languages.getLanguages();
-    await languages.setTextDocumentLanguage(doc, "markdown");
-    await window.showTextDocument(doc, ViewColumn.Active);
+    // const doc = await workspace.openTextDocument(respPath);
+    // await languages.setTextDocumentLanguage(doc, "markdown");
+    // await window.showTextDocument(doc, ViewColumn.Active);
+
+    const uri = Uri.file(respPath);
+
+    // https://github.com/microsoft/vscode/blob/main/extensions/markdown-language-features/src/commands/showPreview.ts
+    await commands.executeCommand("markdown.showPreview", uri);
 }
