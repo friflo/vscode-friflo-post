e9a0ca97738b3c5942db426529cb875746eb1298
Ullrich Praetz <ullrich@praetz.de>
2021-09-28 11:18:08 +0200

    add webpack

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,7 @@
 // Copyright (c) Ullrich Praetz. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-import * as minimatch  from "minimatch";
-
+import minimatch from "minimatch";
 
 // Separate minimatch because of nasty import 
 export function Match (target: string, pattern: string) {
